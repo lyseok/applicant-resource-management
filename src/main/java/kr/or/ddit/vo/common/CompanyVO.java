bd370533a037d15f96da6c0b7ed7f7c19221c3ee
sangyoon-329 <robin0329150@gmail.com>
2025-07-02 16:06:18 +0900

    company VO validate 작성 comImage 추가

--- src/main/java/kr/or/ddit/vo/common/CompanyVO.java
+++ src/main/java/kr/or/ddit/vo/common/CompanyVO.java
@@ -2,21 +2,33 @@ package kr.or.ddit.vo.common;
 
 import java.io.Serializable;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.validation.constraints.NotBlank;
+import kr.or.ddit.validate.constraints.MimeTypeCheck;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(of = "userId")
 public class CompanyVO implements Serializable{
+	@NotBlank
 	private String userId;
+	@NotBlank
 	private String comName;
 	private String comInfo;
+	@NotBlank
 	private String comNum;
+	@NotBlank
 	private String comEmail;
 	private String comUrl;
+	@NotBlank
 	private String comCreateYear;
+	@NotBlank
 	private Integer comMem;
 	private String comLogo;
+	@MimeTypeCheck(mainType = "image/")
+	private MultipartFile comImage;
 	private String comPayment;
 	private String industryType;
 }
