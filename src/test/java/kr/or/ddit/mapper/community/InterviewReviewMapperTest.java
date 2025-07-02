91aada2cb37b71525087b4ed9846d636a1594360
hongtaegyu <hongtaegyu@localhost>
2025-07-03 07:57:15 +0900

    InterviewReviewMapper, test 작

--- src/test/java/kr/or/ddit/mapper/community/InterviewReviewMapperTest.java
+++ src/test/java/kr/or/ddit/mapper/community/InterviewReviewMapperTest.java
@@ -22,7 +22,11 @@ class InterviewReviewMapperTest {
 	void testSelectInterviewList() {
 		String id = "EMP001";
 		
-		assertDoesNotThrow(()->mapper.selectInterviewList(id));
+		mapper.selectInterviewList(id).forEach(iReview ->{
+			log.info("{}", iReview);
+		});
+		
+		
 	}
 	
 	
@@ -51,53 +55,8 @@ class InterviewReviewMapperTest {
 	}
 	
 	
-	@Test 
-	void testInsertInterviewReviewInfromation(){
-		// First insert a review to get a valid interviewReviewNo
-        InterviewReviewVO review = new InterviewReviewVO();
-        review.setInterviewNo("INTV000001");
-        review.setComId("EMP001");
-        review.setJobCode("JOB001");
-        review.setInterviewDate("2025-07-02 10:00");
-      
-        mapper.insertInterviewReview(review);
-        String reviewNo = review.getInterviewReviewNo();
-        
-        // Arrange information
-        InterviewInformationVO info = new InterviewInformationVO();
-        info.setInterviewReviewNo(reviewNo);
-        info.setEvaluation("A");
-        info.setInterviewLevel("1");
-        info.setInterviewType("대");
-        info.setInterviewContent("Detailed content test.");
-
-        // Act
-        int result = mapper.insertInterviewInformation(info);
-
-        // Assert
-        assertEquals(1, result, "insertInterviewInformation should return 1");
-        assertNotNull(info.getInterviewInformationNo(), "interviewInformationNo should be populated");
-        log.info("Inserted information ID: {}", info.getInterviewInformationNo());
-	}
 	
-	@Test 
-	void testInsertPassInfromation(){
 	
-	    PassInformationVO pass = new PassInformationVO();
-	    pass.setInterviewReviewNo("P001");
-	    pass.setInterviewReviewNo("INTV000001");
-	    pass.setInterviewQuestion("면접 질문");
-	    pass.setTip("잘 준비해");
-	    pass.setInterviewPassYn("Y");
-
-
-        // Act
-        int result = mapper.insertPassInformation(pass);
-
-        // Assert
-        assertEquals(1, result, "insertPassInformation should return 1");
-        log.info("Inserted pass information ID: {}", result);
-	}
 	
 	
 	@Test
