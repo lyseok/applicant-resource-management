fca24c4e1547f66ee6a17e751c9aaba782f18603
hongtaegyu <hongtaegyu@localhost>
2025-07-03 07:58:52 +0900

    InterviewInformation mapper작

---
+++ src/main/java/kr/or/ddit/mapper/community/InterviewInformationMapper.java
@@ -0,0 +1,17 @@
+package kr.or.ddit.mapper.community;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.or.ddit.vo.community.InterviewInformationVO;
+
+@Mapper
+public interface InterviewInformationMapper {
+	public List<InterviewInformationVO> selectInterviewInfromationList();
+	
+	public InterviewInformationVO selectInterviewInformationVO();
+	
+	public int insertInterviewInformation(InterviewInformationVO interviewInformation);
+	
+}
