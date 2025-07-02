d5950bcc9db277be4bcd61c53343f8b44d0bfdea
hongtaegyu <hongtaegyu@localhost>
2025-07-03 07:55:39 +0900

    SalaryMapper, test 완

--- src/main/java/kr/or/ddit/mapper/common/SalaryMapper.java
+++ src/main/java/kr/or/ddit/mapper/common/SalaryMapper.java
@@ -1,14 +1,19 @@
 package kr.or.ddit.mapper.common;
 
 import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
 
 import kr.or.ddit.vo.common.SalaryVO;
 
+@Mapper
 public interface SalaryMapper {
 	public List<SalaryVO> selectSalaryList();
 	public SalaryVO selectSalary(String comId);
+	public List<SalaryVO> selectSalaryRangeList(Map<String, Object> params);
+	public List<SalaryVO> selectSalaryComNameList(String comName);
 	public int updateSalary(SalaryVO salary);
 	public int insertSalary(SalaryVO salary);
 	public int deleteSalary(String salaryId);
-
 }
