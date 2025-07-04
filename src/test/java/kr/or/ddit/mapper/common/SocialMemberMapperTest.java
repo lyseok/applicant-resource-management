cc281564d09053ee295be23643fa40e6963e0112
PC-18 <PC-18@DESKTOP-1UO0R1F>
2025-07-04 14:56:09 +0900

    나머지 mapper 작업

--- src/test/java/kr/or/ddit/mapper/common/SocialMemberMapperTest.java
+++ src/test/java/kr/or/ddit/mapper/common/SocialMemberMapperTest.java
@@ -3,32 +3,65 @@ package kr.or.ddit.mapper.common;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import kr.or.ddit.vo.common.Oauth2TokenVO;
+import kr.or.ddit.vo.common.SocialMemberVO;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
 class SocialMemberMapperTest {
+	@Autowired
+	SocialMemberMapper mapper;
 
+	
+	
 	@Test
 	void testSelectSocialMemberById() {
-		fail("Not yet implemented");
+
 	}
 
 	@Test
 	void testSelectSocialMemberList() {
-		fail("Not yet implemented");
+		mapper.selectSocialMemberList().forEach(list ->{
+			log.info("{}", list);
+		});
 	}
+	
+	
+	
 
 	@Test
 	void testInsertSocialMember() {
-		fail("Not yet implemented");
+		SocialMemberVO vo = new SocialMemberVO();
+		vo.setSocialMemId("kakao_9988776655");
+		vo.setUserId("USR002");
+		vo.setSocialId("이게구글아이디인가");
+		
+		mapper.insertSocialMember(vo);
+		
+		SocialMemberVO vo2 = mapper.selectSocialMemberById(vo);
+		log.info("{}", vo2);
 	}
 
 	@Test
 	void testUpdateSocialMember() {
-		fail("Not yet implemented");
+		SocialMemberVO vo = new SocialMemberVO();
+		vo.setSocialMemId("naver_abcdefg987");
+		vo.setUserId("USR002");
+		vo.setSocialId("이게구글");
 	}
 
 	@Test
 	void testDeleteSocialMember() {
-		fail("Not yet implemented");
+		SocialMemberVO vo = new SocialMemberVO();
+		vo.setSocialMemId("naver_abcdefg987");
+		vo.setUserId("USR002");
+		mapper.deleteSocialMember(vo);
+		
+		assertNull(mapper.selectSocialMemberById(vo));
 	}
 
 }
