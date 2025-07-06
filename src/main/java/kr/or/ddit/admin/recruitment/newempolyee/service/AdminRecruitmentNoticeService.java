44926811d48f53228505659dd6515c1fae803187
dhfjdhfj <dhfjdhfj@naver.com>
2025-07-07 01:26:02 +0900

    커밋

---
+++ src/main/java/kr/or/ddit/admin/recruitment/newempolyee/service/AdminRecruitmentNoticeService.java
@@ -0,0 +1,21 @@
+package kr.or.ddit.admin.recruitment.newempolyee.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import kr.or.ddit.vo.recruitment.RecruitmentNoticeVO;
+
+public interface AdminRecruitmentNoticeService {
+	public List<RecruitmentNoticeVO> readRecruitmentNoticeList();
+		// 전체
+	
+	public Optional<RecruitmentNoticeVO> realTimeRecruitment(String recruitmentNo);
+		// 최신순
+	
+	 public List<RecruitmentNoticeVO> salaryRecruitment();
+	 	// 연봉순
+	
+	 public RecruitmentNoticeVO selectliveRecruitmentDetail(String recruitmentNo);
+	 	// 상세정보
+
+}
