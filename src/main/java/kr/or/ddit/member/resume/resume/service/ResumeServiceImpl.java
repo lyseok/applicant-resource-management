d9f1be08c16e867954c494198c6bd4bbb05c4852
yesolee97 <dpthf9596@gmail.com>
2025-07-08 09:44:27 +0900

    서비스impl 작업 커밋

--- src/main/java/kr/or/ddit/member/resume/resume/service/ResumeServiceImpl.java
+++ src/main/java/kr/or/ddit/member/resume/resume/service/ResumeServiceImpl.java
@@ -4,38 +4,60 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import kr.or.ddit.mapper.resume.AwardMapper;
+import kr.or.ddit.mapper.resume.CareerMapper;
+import kr.or.ddit.mapper.resume.LanguageSkillMapper;
+import kr.or.ddit.mapper.resume.MilitaryMapper;
+import kr.or.ddit.mapper.resume.MyExperienceMapper;
+import kr.or.ddit.mapper.resume.MyLicenseMapper;
+import kr.or.ddit.mapper.resume.MySkillMapper;
+import kr.or.ddit.mapper.resume.PortfolioMapper;
 import kr.or.ddit.mapper.resume.ResumeMapper;
+import kr.or.ddit.mapper.resume.SubIntroductionMapper;
+import kr.or.ddit.mapper.resume.SupportMapper;
 import kr.or.ddit.vo.resume.ResumeVO;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class ResumeServiceImpl implements ResumeService {
-	private final ResumeMapper mapper;
+	private final ResumeMapper resumeMapper;
+	private final CareerMapper careerMapper;
+	private final SupportMapper supportMapper;
+	private final AwardMapper awardMapper;
+	private final MyExperienceMapper myExperienceMapper;
+	private final MySkillMapper mySkillMapper;
+	private final MyLicenseMapper myLicenseMapper;
+	private final SubIntroductionMapper subIntroductionMapper;
+	private final LanguageSkillMapper languageSkillMapper;
+	private final PortfolioMapper portfolioMapper;
+	private final MilitaryMapper militaryMapper;
+	
+	
 	
 	@Override
 	public List<ResumeVO> readResumeList() {
-		return mapper.selectResumeList();
+		return resumeMapper.selectResumeList();
 	}
 
 	@Override
 	public ResumeVO readResumeDetail(String no) {
-		return mapper.selectResumeDetail(no);
+		return resumeMapper.selectResumeDetail(no);
 	}
 
 	@Override
 	public void createResume(ResumeVO vo) {
-		mapper.insertResume(vo);
+		resumeMapper.insertResume(vo);
 	}
 
 	@Override
 	public void editResume(ResumeVO vo) {
-		mapper.updateResume(vo);
+		resumeMapper.updateResume(vo);
 	}
 
 	@Override
 	public void removeResume(String no) {
-		mapper.deleteResume(no);
+		resumeMapper.deleteResume(no);
 	}
 
 }
