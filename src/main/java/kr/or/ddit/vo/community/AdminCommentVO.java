4b95462fd67c169627a51cc6d4c90c9c69610203
notbinary-hj <sleepingc0ya@gmail.com>
2025-07-10 19:19:14 +0900

    관리자 답글 등록 폼 만들면서 일반회원 등록 폼도 만들어버렸음

--- src/main/java/kr/or/ddit/vo/community/AdminCommentVO.java
+++ src/main/java/kr/or/ddit/vo/community/AdminCommentVO.java
@@ -19,5 +19,7 @@ public class AdminCommentVO implements Serializable {
 	private String boardDeleteDate;
 	private String boardCommentStatus;
 	
+	private String codeName;  //문의사항에는 공통 코드가 없지만, AdminBoardVO에 AdminComment가 들어있어서 같이 넣어야 함
+	
 	private transient UsersVO users;
 }
