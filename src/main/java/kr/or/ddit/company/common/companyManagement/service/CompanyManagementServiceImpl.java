4bc6dc3f268a561902ad96bf086ddd7b512ca243
PC-18 <PC-18@DESKTOP-1UO0R1F>
2025-07-15 14:10:11 +0900

    커밋

--- src/main/java/kr/or/ddit/company/common/companyManagement/service/CompanyManagementServiceImpl.java
+++ src/main/java/kr/or/ddit/company/common/companyManagement/service/CompanyManagementServiceImpl.java
@@ -1,8 +1,20 @@
 package kr.or.ddit.company.common.companyManagement.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class CompanyManagementServiceImpl {
+import kr.or.ddit.mapper.common.CompanyMapper;
+import kr.or.ddit.vo.common.CompanyVO;
 
+@Service
+public class CompanyManagementServiceImpl implements CompanyManagementService {
+    
+	@Autowired
+	CompanyMapper companyMapper;
+	
+	@Override
+	public CompanyVO readCompanyById(String userId) {
+		return companyMapper.selectCompanyById(userId);
+	}
+	
 }
