8142aedbbb6504bafc7967d7a604389e2d7d08c6
PC-18 <PC-18@DESKTOP-1UO0R1F>
2025-07-16 19:41:01 +0900

    다시 복구

---
+++ src/main/java/kr/or/ddit/company/payment/product/service/PaymentProductService.java
@@ -0,0 +1,19 @@
+package kr.or.ddit.company.payment.product.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import kr.or.ddit.vo.common.PaymentProductVO;
+
+public interface PaymentProductService {
+	public List<PaymentProductVO> selectPaymentProductList();
+
+	public PaymentProductVO selectPaymentProductByPk(PaymentProductVO vo);
+
+	public int insertPaymentProduct(PaymentProductVO vo);
+
+	public int updatePaymentProduct(PaymentProductVO vo);
+
+	public int deletePaymentProduct(String productNo);
+}
