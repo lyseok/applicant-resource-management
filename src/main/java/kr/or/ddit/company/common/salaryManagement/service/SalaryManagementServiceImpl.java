91e1a5f9e2b2d3f99dd35630fd6f12f26ffdd96c
PC-18 <PC-18@DESKTOP-1UO0R1F>
2025-07-16 13:59:26 +0900

    Service 어노테이션 추가

--- src/main/java/kr/or/ddit/company/common/salaryManagement/service/SalaryManagementServiceImpl.java
+++ src/main/java/kr/or/ddit/company/common/salaryManagement/service/SalaryManagementServiceImpl.java
@@ -1,5 +1,8 @@
 package kr.or.ddit.company.common.salaryManagement.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SalaryManagementServiceImpl implements SalaryManagementService{
 
 }
