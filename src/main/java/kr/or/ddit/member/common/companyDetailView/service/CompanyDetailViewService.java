5822f84cfab96b69ef9cacefd3ab4722dffedd08
PC-18 <PC-18@DESKTOP-1UO0R1F>
2025-07-17 16:58:35 +0900

    CompanyVO, mapper.xml 다시 수정,,,,

--- src/main/java/kr/or/ddit/member/common/companyDetailView/service/CompanyDetailViewService.java
+++ src/main/java/kr/or/ddit/member/common/companyDetailView/service/CompanyDetailViewService.java
@@ -1,5 +1,7 @@
 package kr.or.ddit.member.common.companyDetailView.service;
 
+import kr.or.ddit.vo.common.CompanyVO;
+
 public interface CompanyDetailViewService {
-	
+	public CompanyVO readCompanyInfoById(String userId);
 }
