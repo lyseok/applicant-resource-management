2a53447a87578c155a760a03a5cf8842c22e9fc2
yesolee97 <dpthf9596@gmail.com>
2025-07-18 18:27:10 +0900

    이력서 수정로직 구현 완료, 이력서 검색 로직 구현중

--- src/main/resources/static/js/member/resume/resumeList.js
+++ src/main/resources/static/js/member/resume/resumeList.js
@@ -7,3 +7,11 @@ function showLoading() {
 function hideLoading() {
   document.getElementById('loadingSpinner').style.setProperty('display', 'none', 'important');
 }
+
+
+
+document.querySelector('.searchBarBtn').addEventListener('click', function(e) {
+    e.preventDefault();
+    const keyword = document.getElementById('listKeyword').value;
+    window.location.href = '/mypage/resume/search?keyword=' + encodeURIComponent(keyword);
+});
