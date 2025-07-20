2f05c233e330d983a529dede675cdb4b94e6fc26
hongtaegyu <hongtaegyu@localhost>
2025-07-21 02:40:45 +0900

    기업 상세 연봉 구현 중 , alaryMapper, SalaryMapper.xml 추가 수정

--- src/main/java/kr/or/ddit/member/common/companyDetailView/service/CompanyDetailViewService.java
+++ src/main/java/kr/or/ddit/member/common/companyDetailView/service/CompanyDetailViewService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import kr.or.ddit.dto.CompanyOpProfitDTO;
+import kr.or.ddit.dto.CompanySalaryDTO;
 import kr.or.ddit.dto.CompanySalesDTO;
 import kr.or.ddit.vo.common.CompanyVO;
 import kr.or.ddit.vo.recruitment.RecruitmentNoticeVO;
@@ -16,5 +17,7 @@ public interface CompanyDetailViewService {
 	public List<CompanyOpProfitDTO> readCompanyProfitInfoById(String userId);
 	
 	public List<RecruitmentNoticeVO> readRecruitmentNoticeByUserId(String userId);
+	
+	public List<CompanySalaryDTO> readSalaryStatisticsById(String userId);
 	
 }
