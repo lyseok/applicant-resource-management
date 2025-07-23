ccec8edb79a50d0d3c13c720cb7ce90388486999
sangyoon-329 <robin0329150@gmail.com>
2025-07-23 19:54:23 +0900

    개인회원 지원 현황 완성

--- src/main/java/kr/or/ddit/member/recruitment/applicant/service/MemberApplicantService.java
+++ src/main/java/kr/or/ddit/member/recruitment/applicant/service/MemberApplicantService.java
@@ -1,14 +1,20 @@
 package kr.or.ddit.member.recruitment.applicant.service;
 
 import java.util.List;
+import java.util.Map;
 
 import kr.or.ddit.mapper.recruitment.ApplicantMapper;
 import kr.or.ddit.vo.recruitment.ApplicantVO;
+import kr.or.ddit.vo.recruitment.PasserVO;
+import kr.or.ddit.vo.recruitment.RecruitmentNoticeVO;
 
 public interface MemberApplicantService {
 	public List<ApplicantVO> selectApplicantList();
+	public List<Map<String, Object>> readApplicatedList();
+	public List<Map<String, Object>> readMyApplicatedStep();
 	public ApplicantVO selectApplicant(String applicantId);
 	public int insertApplicant(ApplicantVO vo);
 	public int updateApplicant(ApplicantVO vo);
+	public int updateAccept(PasserVO vo);
 	public int deleteApplicant(String applicantId);
 }
