7bce92f25c23a4924ac7122713c36240d108f4dc
'dhfjdhfj' <'dhfjdhfj@naver.com'>
2025-07-24 19:44:45 +0900

    우선 중간커밋

---
+++ src/main/java/kr/or/ddit/company/payment/log/controller/PaymentLogController.java
@@ -0,0 +1,13 @@
+package kr.or.ddit.company.payment.log.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@RequestMapping
+@Controller
+public class PaymentLogController {
+	
+}
--- src/main/resources/static/js/company/payment/Addproduct.js
+++ src/main/resources/static/js/company/payment/Addproduct.js
@@ -3,19 +3,26 @@ function selectType(type) {
 	document.getElementById("formCard").classList.remove("d-none");
 
 	// 유형 저장
-	document.getElementById("productType").value = type;
+	document.getElementById("productType").value = type === "정기권" ? 'L' : 'S';
 
 	// 카드 선택 스타일 적용
 	document.getElementById("card-regular").classList.remove("selected");
 	document.getElementById("card-single").classList.remove("selected");
 
-	if (type === "정기권") {
+	if (type === '정기권') {
 		document.getElementById("card-regular").classList.add("selected");
-		localStorage.setItem("selectedType" , "정기권");
+		document.getElementById('productPeriodDiv').style.display = 'block'; // 기간 설정 div 표시
+		localStorage.setItem("selectedType", "정기권");
 	} else {
 		document.getElementById("card-single").classList.add("selected");
+		document.getElementById('productPeriodDiv').style.display = 'none'; // 기간 설정 div 숨기기
 		localStorage.setItem("selectedType" , "단건");
 	}
+
+	// 공통 사항인 'PRODUCT_LIMIT' 설정을 표시
+	document.getElementById('productLimitDiv').style.display = 'block'; // 한도 설정 div 표시
+
+	
 
 	// 제목에 선택된 유형 표시
 	document.getElementById("selectedType").textContent = `(${type})`;
