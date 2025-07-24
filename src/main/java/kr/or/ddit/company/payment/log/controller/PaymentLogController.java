a60457017f8544fd916b94da318514b749865aea
'dhfjdhfj' <'dhfjdhfj@naver.com'>
2025-07-24 19:51:46 +0900

    사이드메뉴 수정

--- src/main/java/kr/or/ddit/company/payment/log/controller/PaymentLogController.java
+++ src/main/java/kr/or/ddit/company/payment/log/controller/PaymentLogController.java
@@ -1,13 +1,18 @@
 package kr.or.ddit.company.payment.log.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
-@RequestMapping
 @Controller
+@RequestMapping("/company/log")
 public class PaymentLogController {
-	
+
+	@GetMapping("/list")
+	public String listFormUI() {
+		return "";
+	}
 }
