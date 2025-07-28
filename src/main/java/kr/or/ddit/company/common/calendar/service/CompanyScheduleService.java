dd015a345c25b249c57833fbcd08a2d80c85008b
sangyoon-329 <robin0329150@gmail.com>
2025-07-28 12:28:02 +0900

    기업페이지 calendar 구현

---
+++ src/main/java/kr/or/ddit/company/common/calendar/service/CompanyScheduleService.java
@@ -0,0 +1,9 @@
+package kr.or.ddit.company.common.calendar.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CompanyScheduleService {
+
+	public List<Map<String, Object>> getCompanySchedule();
+}
