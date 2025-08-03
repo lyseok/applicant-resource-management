ba20365de422fba25b20a43a0c819d30c882d404
YUN-SEOK LEE <115852659+lyseok@users.noreply.github.com>
2025-08-04 02:21:43 +0900

    템플릿 리스트 수정

--- src/main/resources/static/js/company/recruitment/mailTemplate/mailTemplateList.js
+++ src/main/resources/static/js/company/recruitment/mailTemplate/mailTemplateList.js
@@ -47,10 +47,6 @@ document.addEventListener('DOMContentLoaded', function () {
         resumeContainer.innerHTML = `
           <div class="intoructionList p-6 d-flex flex-column align-items-center gap-2">
             <span>띹잡에 등록된 템플릿이 없어요!</span>
-            <a class="fw-bold d-flex" href="/mypage/resume/create">
-              템플릿 등록하러 가기
-              <span class="material-symbols-outlined">chevron_right</span>
-            </a>
           </div>
         `;
         return;
