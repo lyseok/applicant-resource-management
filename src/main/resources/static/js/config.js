934f851cc56192c119516b4dc12c3056904626b6
YUN-SEOK LEE <115852659+lyseok@users.noreply.github.com>
2025-08-04 18:02:22 +0900

    Merge branch 'develop' of https://github.com/lyseok/applicant-resource-management.git into feature/#041_데이터

--- src/main/resources/static/js/config.js
+++ src/main/resources/static/js/config.js
@@ -1,5 +1,5 @@
 // config.js
 window.APP_CONFIG = window.APP_CONFIG || {
-  BASE_URL: 'http://192.168.34.70:3000/',
-  REACT_BASE_URL: 'http://192.168.34.70:3000/',
+  BASE_URL: 'http://localhost:3000/',
+  REACT_BASE_URL: 'http://localhost:3000/',
 };
